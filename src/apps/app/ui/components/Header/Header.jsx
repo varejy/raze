5ecd3e57976ae44f4c43e619f32a37a41d07a304
VictoriaVM <victoria.v.movchan@gmail.com>
2019-06-07 16:53:52 +0300

    Выполнила правки.

--- src/apps/app/ui/components/Header/Header.jsx
+++ src/apps/app/ui/components/Header/Header.jsx
@@ -3,56 +3,56 @@ import styles from './Header.css';
 
 class Header extends Component {
     render () {
-        return <div className={styles.header_container}>
-            <div className={styles.header_top}>
+        return <div className={styles.headerContainer}>
+            <div className={styles.headerTop}>
                 <div className={styles.logo}>
-                    <span className={styles.logo_left}>raze</span>
-                    <span className={styles.logo_right}>Your<br/>knife<br/><span className={styles.logo_green}>world</span></span>
+                    <span className={styles.logoLeft}>raze</span>
+                    <span className={styles.logoRight}>Your<br/>knife<br/><span className={styles.logoGreen}>world</span></span>
                 </div>
-                <div className={styles.search_form}>
-                    <input value='Поиск продуктов...'/>
-                    <button className={styles.search_form_icon}><img src='../../../../../../client/images/Search.png' alt=''/></button>
+                <div className={styles.searchForm}>
+                    <input className={styles.searchFormInput} value='Поиск продуктов...'/>
+                    <button className={styles.searchFormIcon}><img src='/src/apps/app/ui/components/Header/images/search.png' alt=''/></button>
                 </div>
-                <div className={styles.contacts_wrapper}>
+                <div className={styles.contactsWrapper}>
                     <div className={styles.contacts}>
-                        <div className={styles.contacts_license}>
+                        <div className={styles.contactsLicense}>
                             <span>Лицензионное соглашение</span>
                         </div>
-                        <div className={styles.toll_email}>
+                        <div className={styles.tollEmail}>
                             <div className={styles.toll}>
-                                <div className={styles.icon_wrapper}>
-                                    <img className={styles.icon_phone} src='../../../../../../client/images/iPhone.png' alt=''/>
-                                    <img className={styles.color_element} src='../../../../../../client/images/color-element-1.png' alt=''/>
+                                <div className={styles.iconWrapper}>
+                                    <img className={styles.iconPhone} src='/src/apps/app/ui/components/Header/images/iPhone.png' alt=''/>
+                                    <img className={styles.colorElement} src='/src/apps/app/ui/components/Header/images/colorElement1.png' alt=''/>
                                 </div>
-                                <span>+38 (044) 232 13 14</span>
+                                <a href="tel:+38 (044) 232 13 14" className={styles.link}>+38 (044) 232 13 14</a>
                             </div>
                             <div className={styles.email}>
-                                <div className={styles.icon_wrapper}>
-                                    <img className={styles.icon_mail} src='../../../../../../client/images/Mail.png' alt=''/>
-                                    <img className={styles.color_element} src='../../../../../../client/images/color-element-2.png' alt=''/>
+                                <div className={styles.iconWrapper}>
+                                    <img className={styles.iconMail} src='/src/apps/app/ui/components/Header/images/mail.png' alt=''/>
+                                    <img className={styles.colorElement} src='/src/apps/app/ui/components/Header/images/colorElement2.png' alt=''/>
                                 </div>
-                                <span>info@oneblade.org</span>
+                                <a href="mailto:info@oneblade.org" className={styles.link}>info@oneblade.org</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className={styles.header_bottom}>
+            <div className={styles.headerBottom}>
                 <div className={styles.menu}>
-                    <ul>
-                        <li>Ножи</li>
-                        <li>Топоры</li>
-                        <li>Точила</li>
-                        <li>Аксессуары</li>
+                    <ul className={styles.menuList}>
+                        <li className={styles.menuListCategory}>Ножи</li>
+                        <li className={styles.menuListCategory}>Топоры</li>
+                        <li className={styles.menuListCategory}>Точила</li>
+                        <li className={styles.menuListCategory}>Аксессуары</li>
                     </ul>
                 </div>
-                <div className={styles.likes_basket}>
-                    <div className={styles.bottom_icon_wrapper}>
-                        <img className={styles.icon_heart} src='../../../../../../client/images/like-heart.png' alt=''/>
+                <div className={styles.likesBasket}>
+                    <div className={styles.bottomIconWrapper}>
+                        <img className={styles.iconHeart} src='/src/apps/app/ui/components/Header/images/likeHeart.png' alt=''/>
                     </div>
-                    <div className={styles.bottom_icon_wrapper}>
-                        <img className={styles.icon_basket} src='../../../../../../client/images/basket.png' alt=''/>
-                        <div className={styles.orders_counter}><span>3</span></div>
+                    <div className={styles.bottomIconWrapper}>
+                        <img className={styles.iconBasket} src='/src/apps/app/ui/components/Header/images/basket.png' alt=''/>
+                        <div className={styles.ordersCounter}><span className={styles.ordersNumber}>3</span></div>
                     </div>
                 </div>
             </div>
