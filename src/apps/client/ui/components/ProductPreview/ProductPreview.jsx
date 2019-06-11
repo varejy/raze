ca9902e3fcc030dcaf8861933a1aa5492b945d63
VictoriaVM <victoria.v.movchan@gmail.com>
2019-06-11 10:10:25 +0300

    поправила слайдер, карточку товара

--- src/apps/client/ui/components/ProductPreview/ProductPreview.jsx
+++ src/apps/client/ui/components/ProductPreview/ProductPreview.jsx
@@ -2,7 +2,39 @@ import React, { Component } from 'react';
 import styles from './ProductPreview.css';
 import classNames from 'classnames';
 
-const PREVIEW_WIDTH = 696;
+const PARAMETERS = [
+    {
+        parameterName: 'Лезвие',
+        parameterValue: 'Сталь'
+    },
+    {
+        parameterName: 'Длина',
+        parameterValue: '25 см'
+    },
+    {
+        parameterName: 'Ширина',
+        parameterValue: '15 см'
+    }
+];
+
+const PRODUCT_INFO = {
+    productName: 'название товара',
+    productDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\n' +
+        '                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse\n' +
+        '                            ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.'
+};
+
+const SLIDER_IMAGES = [
+    { path: '/src/apps/client/ui/components/ProductPreview/images/ontario1.jpg' },
+    { path: '/src/apps/client/ui/components/ProductPreview/images/ontario2.jpg' },
+    { path: '/src/apps/client/ui/components/ProductPreview/images/ontario3.jpg' },
+    { path: '/src/apps/client/ui/components/ProductPreview/images/ontario4.jpg' },
+    { path: '/src/apps/client/ui/components/ProductPreview/images/ontario5.jpg' }
+];
+
+const PREVIEW_WIDTH = 700;
+
+const SLIDES_QUANTITY = SLIDER_IMAGES.length;
 
 class ProductPreview extends Component {
     state = {
@@ -32,68 +64,47 @@ class ProductPreview extends Component {
             <div className={styles.productPreview}>
                 <div className={styles.productPhotoContainer}>
                     <div className={styles.slides} style={{ left: `-${this.state.leftPosition.toString()}px` }}>
-                        <div className={styles.productPreviewSlide}>
-                            <img className={styles.slidePhoto} src='/src/apps/client/ui/components/ProductPreview/images/ontario1.jpg' alt=''/>
-                        </div>
-                        <div className={styles.productPreviewSlide}>
-                            <img className={styles.slidePhoto} src='/src/apps/client/ui/components/ProductPreview/images/ontario2.jpg' alt=''/>
-                        </div>
-                        <div className={styles.productPreviewSlide}>
-                            <img className={styles.slidePhoto} src='/src/apps/client/ui/components/ProductPreview/images/ontario3.jpg' alt=''/>
-                        </div>
-                        <div className={styles.productPreviewSlide}>
-                            <img className={styles.slidePhoto} src='/src/apps/client/ui/components/ProductPreview/images/ontario4.jpg' alt=''/>
-                        </div>
-                        <div className={styles.productPreviewSlide}>
-                            <img className={styles.slidePhoto} src='/src/apps/client/ui/components/ProductPreview/images/ontario5.jpg' alt=''/>
-                        </div>
+                        {SLIDER_IMAGES.map((SLIDER_IMAGE, i) =>
+                            <div className={styles.productPreviewSlide} key={i}>
+                                <img className={styles.slidePhoto} src={SLIDER_IMAGE.path} alt={`slide${i}`} />
+                            </div>)}
                     </div>
-                    <button className={classNames(styles.buttonLeft, this.state.leftPosition === 0 && styles.buttonDisabled)}
-                        onClick={this.state.leftPosition !== 0 && this.handleArrowClick('left')}>
+                    <button
+                        className={classNames(styles.buttonLeft, this.state.leftPosition === 0 && styles.buttonDisabled)}
+                        onClick={this.state.leftPosition !== 0 && this.handleArrowClick('left')}
+                    >
                         <img src='/src/apps/client/ui/components/ProductPreview/images/arrowLeft.png' alt=''/>
                     </button>
-                    <button className={classNames(styles.buttonRight, this.state.leftPosition === (PREVIEW_WIDTH * 4) && styles.buttonDisabled)}
-                        onClick={this.state.leftPosition !== (PREVIEW_WIDTH * 4) && this.handleArrowClick('right')}>
+                    <button
+                        className={classNames(styles.buttonRight, this.state.leftPosition === (PREVIEW_WIDTH * (SLIDES_QUANTITY - 1)) && styles.buttonDisabled)}
+                        onClick={this.state.leftPosition !== (PREVIEW_WIDTH * (SLIDES_QUANTITY - 1)) && this.handleArrowClick('right')}
+                    >
                         <img src='/src/apps/client/ui/components/ProductPreview/images/arrowRight.png' alt=''/>
                     </button>
                     <div className={styles.dotsContainer}>
                         <div className={styles.buttonDots}>
-                            <div className={classNames(styles.dot, this.state.leftPosition === 0 && styles.dotActive)}
-                                onClick={this.handleDotClick(0)}/>
-                            <div className={classNames(styles.dot, this.state.leftPosition === PREVIEW_WIDTH && styles.dotActive)}
-                                onClick={this.handleDotClick(PREVIEW_WIDTH * 1)}/>
-                            <div className={classNames(styles.dot, this.state.leftPosition === (PREVIEW_WIDTH * 2) && styles.dotActive)}
-                                onClick={this.handleDotClick(PREVIEW_WIDTH * 2)}/>
-                            <div className={classNames(styles.dot, this.state.leftPosition === (PREVIEW_WIDTH * 3) && styles.dotActive)}
-                                onClick={this.handleDotClick(PREVIEW_WIDTH * 3)}/>
-                            <div className={classNames(styles.dot, styles.lastChildDot, this.state.leftPosition === (PREVIEW_WIDTH * 4) && styles.dotActive)}
-                                onClick={this.handleDotClick(PREVIEW_WIDTH * 4)}/>
+                            {SLIDER_IMAGES.map((SLIDER_IMAGE, i) =>
+                                <div key={i} className={classNames(styles.dot, this.state.leftPosition === (PREVIEW_WIDTH * i) && styles.dotActive)}
+                                    onClick={this.handleDotClick(PREVIEW_WIDTH * i)}/>
+                            )}
                         </div>
                     </div>
                 </div>
                 <div className={styles.productInfoContainer}>
                     <div className={styles.productPreviewHeader}>
-                        <div className={styles.productName}>название товара</div>
-                        <div className={styles.likeIcon}><img src="/src/apps/client/ui/components/ProductPreview/images/heartIcon.png" alt=''/></div>
+                        <div className={styles.productName}>{PRODUCT_INFO.productName}</div>
+                        <div className={styles.likeIcon}><img src='/src/apps/client/ui/components/ProductPreview/images/heartIcon.png' alt='' /></div>
                     </div>
                     <div className={styles.productPreviewInfo}>
                         <div className={styles.parameters}>
-                            <div className={styles.parameterLine}>
-                                <div className={styles.parameterName}>Лезвие</div>
-                                <div className={styles.parameterValue}>Сталь</div>
-                            </div>
-                            <div className={styles.parameterLine}>
-                                <div className={styles.parameterName}>Длина</div>
-                                <div className={styles.parameterValue}>25 см</div>
-                            </div>
-                            <div className={styles.parameterLine}>
-                                <div className={styles.parameterName}>Ширина</div>
-                                <div className={styles.parameterValue}>15 см</div>
-                            </div>
+                            {PARAMETERS.map((PARAMETER, i) =>
+                                <div key={i} className={styles.parameterLine}>
+                                    <div className={styles.parameterName}>{PARAMETER.parameterName}</div>
+                                    <div className={styles.parameterValue}>{PARAMETER.parameterValue}</div>
+                                </div>
+                            )}
                         </div>
-                        <div className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse
-                            ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.
+                        <div className={styles.description}>{PRODUCT_INFO.productDescription}
                         </div>
                     </div>
                     <div className={styles.buttonContainer}>
