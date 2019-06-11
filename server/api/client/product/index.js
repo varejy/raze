ed3e397cbf6dee8c3b3ae5bdf3760c0170a9e564
Belous <v.bilous@andersenlab.com>
2019-06-11 19:32:49 +0300

    Добавлено апи поиска

--- server/api/client/product/index.js
+++ server/api/client/product/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 
 import getAvailableProductsByCategoryId from './services/getAvailableProductsByCategoryId';
+import availableProductsSearch from './services/availableProductsSearch';
 
 const router = express.Router();
 
 router.route('/by-category-id')
     .get(getAvailableProductsByCategoryId);
+
+router.route('/search')
+    .get(availableProductsSearch);
 
 export default router;
