1b5d0a7823cd554df31d9a1a2ebc05a0677f0436
VictoriaVM <victoria.v.movchan@gmail.com>
2019-06-14 19:03:17 +0300

    сделала верстку поп-апа корзины

--- src/apps/client/ui/components/PopupBasket/PopupBasket.jsx
+++ src/apps/client/ui/components/PopupBasket/PopupBasket.jsx
@@ -6,63 +6,53 @@ import styles from './PopupBasket.css';
 
 class PopupBasket extends Component {
     render () {
-        return <section>
-            <div className={styles.root}>
+        return <div className={styles.root}>
+            <div className={styles.popupContent}>
                 <div>
-                    <div className={styles.item}>
-                        <div className={styles.itemImageWrapp}>
-                            <div className={styles.deleteItem}>
-                                <span className={styles.deleteItemIcon}></span>
+                    <div className={styles.headerContainer}>
+                        <div className={styles.header}>корзина</div>
+                        <div className={styles.closeButton}>+</div>
+                    </div>
+                    <div className={styles.amountTxt}>
+                        <div>Количество</div>
+                    </div>
+                    <div className={styles.items}>
+                        <div className={styles.item}>
+                            <div className={styles.itemImageWrapp}>
+                                <div className={styles.deleteItem}>
+                                    <img src='/src/apps/client/ui/components/PopupBasket/img/deleteIcon.png'/>
+                                </div>
+                                <div className={styles.itemImage}>
+                                    <img className={styles.itemAvatar}
+                                        src="/src/apps/client/ui/components/PopupBasket/img/BestKnife.jpg"
+                                        alt="product"/>
+                                </div>
                             </div>
-                            <img className={styles.itemImage} src="/src/apps/client/ui/components/PopupBasket/img/BestKnife.jpg" alt="product"/>
-                        </div>
-                        <div className={styles.itemInfo}>
-                            <h2 className={styles.itemName}>Название товара</h2>
-                            <div className={styles.itemCategory}>категория</div>
-                            <h2 className={styles.itemPrice}>1000 UAH</h2>
-                        </div>
-                        <div className={styles.itemAmount}>
-                            <div className={styles.amountTxt}>Количество</div>
-                            <div className={styles.amount}>
+                            <div className={styles.itemInfo}>
+                                <h2 className={styles.itemName}>Название товара</h2>
+                                <div className={styles.itemCategory}>категория</div>
+                                <h2 className={styles.itemPrice}>1000 UAH</h2>
+                            </div>
+                            <div className={styles.itemAmount}>
                                 <span className={styles.amountButton}>-</span>
                                 <div className={styles.countWrapp}>2</div>
                                 <span className={styles.amountButton}>+</span>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className={styles.previouslyAdded}>
-                    <h1 className={styles.previouslyAddedTitle}>РАНЕЕ ДОБАВЛЕННЫЕ</h1>
-                    <div className={styles.previouslyAddedItemWrapp}>
-                        <div className={styles.item}>
-                            <div className={styles.itemImageWrapp}>
-                                <div className={styles.deleteItem}>
-                                    <span className={styles.deleteItemIcon}></span>
-                                </div>
-                                <img className={styles.itemImage} src="/src/apps/client/ui/components/PopupBasket/img/BestKnife.jpg" alt="product"/>
-                            </div>
-                            <div className={styles.itemInfo}>
-                                <h2 className={styles.itemName}>Название товара</h2>
-                                <div className={styles.itemCategory}>категория</div>
-                                <h2 className={styles.itemPrice}>1200 UAH</h2>
-                            </div>
-                            <div className={styles.itemAmount}>
-                                <div className={styles.amountTxt}>Количество</div>
-                                <div className={styles.amount}>
-                                    <span className={styles.amountButton}>-</span>
-                                    <div className={styles.countWrapp}>5</div>
-                                    <span className={styles.amountButton}>+</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <div className={styles.priceTotal}>Итог: 4800 грн</div>
                 </div>
                 <div className={styles.buttonsWrapp}>
-                    <button className={classNames(styles.buttonDefault, styles.continueShopping, styles.buttons)}>продолжить покупки</button>
-                    <button className={classNames(styles.buttonDefault, styles.ordering, styles.buttons)}>оформление заказа</button>
+                    <button
+                        className={classNames(styles.buttonDefault, styles.continueShopping, styles.buttons)}>продолжить
+                        покупки
+                    </button>
+                    <button className={classNames(styles.buttonDefault, styles.ordering, styles.buttons)}>оформление
+                        заказа
+                    </button>
                 </div>
             </div>
-        </section>;
+        </div>;
     }
 }
 
