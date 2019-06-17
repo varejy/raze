9c3d1ba645c5da7500df42ed1094d0727c2ecfad
Belous <v.bilous@andersenlab.com>
2019-06-17 22:55:14 +0300

    Апи для получения продуктов по ids

--- server/api/client/product/index.js
+++ server/api/client/product/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import getAvailableProduct from './services/getAvailableProduct';
+import getAvailableProductsByIds from './services/getAvailableProductsByIds';
 import getAvailableProductsByCategory from './services/getAvailableProductsByCategory';
 import availableProductsSearch from './services/availableProductsSearch';
 
@@ -8,6 +9,9 @@ const router = express.Router();
 
 router.route('/')
     .get(getAvailableProduct);
+
+router.route('/by-ids')
+    .post(getAvailableProductsByIds);
 
 router.route('/by-category')
     .get(getAvailableProductsByCategory);
