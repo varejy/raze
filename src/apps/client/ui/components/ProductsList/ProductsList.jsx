db3f37a04a7be027f65568c725e368548a4d7549
VictoriaVM <victoria.v.movchan@gmail.com>
2019-07-02 17:57:24 +0300

    Добавлена новая логика рендера сортировки.

--- src/apps/client/ui/components/ProductsList/ProductsList.jsx
+++ src/apps/client/ui/components/ProductsList/ProductsList.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import find from '@tinkoff/utils/array/find';
+import findIndex from '@tinkoff/utils/array/findIndex';
+import remove from '@tinkoff/utils/array/remove';
 
 import Product from '../Product/Product';
 
@@ -12,19 +14,31 @@ import styles from './ProductsList.css';
 
 const SORTING_OPTIONS = [
     {
-        text: 'Дате',
-        id: 'date',
+        text: 'От новых к старым',
+        id: 'dateNew',
         min: (product, nextProduct) => product.date - nextProduct.date,
         max: (product, nextProduct) => product.date + nextProduct.date
     },
     {
-        text: 'Цене',
-        id: 'price',
+        text: 'От старых к новым',
+        id: 'dateOld',
+        min: (product, nextProduct) => nextProduct.date - product.date,
+        max: (product, nextProduct) => nextProduct.date + product.date
+    },
+    {
+        text: 'От дешевых к дорогим',
+        id: 'priceMin',
         min: (product, nextProduct) => (product.discountPrice || product.price) - (nextProduct.discountPrice || nextProduct.price),
         max: (product, nextProduct) => (product.discountPrice || product.price) + (nextProduct.discountPrice || nextProduct.price)
     },
     {
-        text: 'Популярности',
+        text: 'От дорогих к дешевым',
+        id: 'priceMax',
+        min: (product, nextProduct) => (nextProduct.discountPrice || nextProduct.price) - (product.discountPrice || product.price),
+        max: (product, nextProduct) => (nextProduct.discountPrice || nextProduct.price) + (product.discountPrice || product.price)
+    },
+    {
+        text: 'По популярности',
         id: 'view',
         min: (product, nextProduct) => product.views - nextProduct.views,
         max: (product, nextProduct) => product.views + nextProduct.views
@@ -67,30 +81,43 @@ class ProductsList extends Component {
             products: products.sort(sortOption.min),
             activeOption: activeOption
         });
-    }
+    };
+
+    getActiveOption = () => {
+        const activeSort = find(sort => sort.id === this.state.activeOption, SORTING_OPTIONS);
+        return !activeSort ? SORTING_OPTIONS[0] : activeSort;
+    };
+
+    renderSorting = () => {
+        const index = findIndex(sort => sort === this.getActiveOption(), SORTING_OPTIONS);
+        return [
+            ...remove(index, 1, SORTING_OPTIONS)
+        ];
+    };
 
     render () {
-        const { category, products, activeOption } = this.state;
+        const { category, products } = this.state;
 
         return <section className={styles.root}>
             <div className={styles.productsFilter}>
                 <div className={styles.filter}>
-                    <div>Сортировать по:</div>
+                    <div>Сортировать:</div>
                     <div className={styles.filterWrapp}>
-                        {
-                            SORTING_OPTIONS.map((option, i) => {
-                                const isActive = activeOption === option.id;
-
-                                return (
-                                    <div
-                                        key={i}
-                                        className={classNames(styles.filterItem, { [styles.filterItemActive]: isActive })}
-                                        onClick={this.handleActiveSortClick(option.id)}>
-                                        {option.text}
-                                    </div>
-                                );
-                            })
-                        }
+                        <ul className={styles.sortingOptions}>
+                            <li className={classNames(styles.filterItem, styles.activeFilterItem)}>
+                                <div className={styles.activeOption}>{this.getActiveOption().text}</div>
+                                <div className={styles.arrowButton}>
+                                    <img src='/src/apps/client/ui/components/ProductsList/tempImages/sortingArrow.png' alt='arrow'/>
+                                </div>
+                            </li>
+                            { this.renderSorting().map((option, i) =>
+                                <li
+                                    key={i}
+                                    className={styles.filterItem}
+                                    onClick={this.handleActiveSortClick(option.id)}>
+                                    {option.text}
+                                </li>)}
+                        </ul>
                     </div>
                 </div>
             </div>
