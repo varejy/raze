6f94fad93c1b082cc60f200dabf303cfe2b8012f
varejy <lionvasia@mail.ru>
2019-07-08 11:57:47 +0300

    comments edits

--- server/api/admin/category/services/editCategory.js
+++ server/api/admin/category/services/editCategory.js
@@ -11,11 +11,7 @@ import { OKEY_STATUS_CODE, SERVER_ERROR_STATUS_CODE } from '../../../../constant
 export default function editCategory (req, res) {
     const { name, path, hidden, id, filters } = req.body;
 
-<<<<<<< HEAD
-    const addIdsFilters = map(filter => {
-=======
     const filtersWithIds = map(filter => {
->>>>>>> c41d644da3ca5076108899a4f22e90bd27211408
         return {
             name: filter.name,
             type: filter.type,
