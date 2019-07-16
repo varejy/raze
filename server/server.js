4bb8e99c49614269f6c3e5a895cbc78d42298341
Belous <v.bilous@andersenlab.com>
2019-07-16 23:39:24 +0300

    https

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import React from 'react';
 import path from 'path';
+import fs from 'fs';
+import https from 'https';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
 import compression from 'compression';
 import expressStaticGzip from 'express-static-gzip';
+import { redirectToHTTPS } from 'express-http-to-https';
 import { renderToString } from 'react-dom/server';
 
 import map from '@tinkoff/utils/array/map';
@@ -35,8 +38,19 @@ import { StaticRouter } from 'react-router-dom';
 import Helmet from 'react-helmet';
 import App from '../src/apps/client/App.jsx';
 
+const credentials = {
+    key: fs.readFileSync('server/https/private.key'),
+    cert: fs.readFileSync('server/https/raze_com_ua.crt'),
+    ca: [
+        fs.readFileSync('server/https/AddTrust_External_CA_Root.crt'),
+        fs.readFileSync('server/https/USERTrust_RSA_Certification_Authority.crt')
+    ]
+};
+const ignoreHttpsHosts = [/localhost:(\d{4})/];
+
 const rootPath = path.resolve(__dirname, '..');
 const PORT = process.env.PORT || 3000;
+const HTTPS_PORT = 443;
 const app = express();
 
 // mongodb
@@ -44,6 +58,9 @@ mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useFindAndModify: false,
 
 // verification
 app.use(verification);
+
+// redirects
+app.use(redirectToHTTPS(ignoreHttpsHosts, [], 301));
 
 // static
 app.get(/\.chunk\.(js|css)$/, expressStaticGzip(rootPath, {
@@ -110,3 +127,7 @@ app.get('*', function (req, res) {
 app.listen(PORT, function () {
     console.log('listening on port', PORT); // eslint-disable-line no-console
 });
+
+const httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(HTTPS_PORT);
