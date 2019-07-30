0e2ede89da9b547f6973bf7342764b798a7006fa
VictoriaVM <victoria.v.movchan@gmail.com>
2019-07-30 18:00:28 +0300

    Рефакторинг

---
+++ src/apps/admin/ui/pages/SeoPage/Accordion.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Typography from '@material-ui/core/Typography';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import MetaForm from './MetaForm';
+
+const materialStyles = () => ({
+    root: {
+        width: '100%'
+    },
+    heading: {
+        fontSize: '15px',
+        flexBasis: '33.33%',
+        flexShrink: 0
+    }
+});
+
+class Accordion extends Component {
+    static propTypes = {
+        classes: PropTypes.object.isRequired
+    };
+
+    constructor (props) {
+        super(props);
+        this.state = {
+            panel1: false,
+            panel2: false,
+            panel3: false
+        };
+    }
+
+    handleChange = panel => () => {
+        this.setState({ [panel]: this.state[panel] !== true });
+    };
+
+    render () {
+        const { classes } = this.props;
+
+        return <div className={classes.root}>
+            <ExpansionPanel expanded={this.state.panel1} onChange={this.handleChange('panel1')}>
+                <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon/>}
+                    aria-controls="panel1bh-content"
+                    id="panel1bh-header"
+                >
+                    <Typography className={classes.heading}>Главная страница</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    <MetaForm/>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+            <ExpansionPanel expanded={this.state.panel2} onChange={this.handleChange('panel2')}>
+                <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon/>}
+                    aria-controls="panel2bh-content"
+                    id="panel2bh-header"
+                >
+                    <Typography className={classes.heading}>Страница заказа</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    <MetaForm/>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+            <ExpansionPanel expanded={this.state.panel3} onChange={this.handleChange('panel3')}>
+                <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon/>}
+                    aria-controls="panel3bh-content"
+                    id="panel3bh-header"
+                >
+                    <Typography className={classes.heading}>Страница поиска</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    <MetaForm/>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+        </div>;
+    }
+}
+
+export default (withStyles(materialStyles)(Accordion));
---
+++ src/apps/admin/ui/pages/SeoPage/MetaForm.jsx
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import TextField from '@material-ui/core/TextField';
+import Tooltip from '@material-ui/core/Tooltip';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import Chip from '@material-ui/core/Chip';
+import AutoRenew from '@material-ui/icons/AutorenewRounded';
+import { withStyles } from '@material-ui/core';
+
+const materialStyles = () => ({
+    metaContainer: {
+        width: '100%'
+    },
+    metaForm: {
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    metaAdd: {
+        marginLeft: '12px',
+        marginTop: '8px'
+    },
+    metaAddKeywords: {
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    metaKeyword: {
+        margin: '4px',
+        marginBottom: '20px'
+    }
+});
+
+class MetaForm extends Component {
+    static propTypes = {
+        classes: PropTypes.object.isRequired
+    };
+
+    render () {
+        const { classes } = this.props;
+
+        return <div className={classes.metaContainer}>
+            <div className={classes.metaForm}>
+                <TextField
+                    label='Title'
+                    value=''
+                    margin='normal'
+                    variant='outlined'
+                    fullWidth
+                    required
+                />
+                <div className={classes.metaAdd}>
+                    <Tooltip
+                        title="Добавить значение по умолчанию"
+                        placement='bottom'
+                    >
+                        <Fab
+                            color="primary"
+                            size='small'
+                        >
+                            <AutoRenew/>
+                        </Fab>
+                    </Tooltip>
+                </div>
+            </div>
+            <div className={classes.metaForm}>
+                <TextField
+                    label='Description'
+                    value=''
+                    margin='normal'
+                    variant='outlined'
+                    fullWidth
+                    required
+                />
+                <div className={classes.metaAdd}>
+                    <Tooltip
+                        title="Добавить значение по умолчанию"
+                        placement='bottom'
+                    >
+                        <Fab
+                            color="primary"
+                            size='small'
+                        >
+                            <AutoRenew/>
+                        </Fab>
+                    </Tooltip>
+                </div>
+            </div>
+            <div className={classes.metaAddKeywords}>
+                <TextField
+                    label='Новое ключевое слово'
+                    value=''
+                    margin='normal'
+                    variant='outlined'
+                    fullWidth
+                />
+                <div className={classes.metaAdd}>
+                    <Tooltip title='Добавить ключевое слово' placement='bottom'>
+                        <Fab size='small' color='primary' aria-label="Add">
+                            <AddIcon/>
+                        </Fab>
+                    </Tooltip>
+                </div>
+                <div className={classes.metaAdd}>
+                    <Tooltip
+                        title="Добавить ключевые слова по умолчанию"
+                        placement='bottom'
+                    >
+                        <Fab
+                            size='small'
+                            color='primary'
+                            aria-label="Add"
+                        >
+                            <AutoRenew/>
+                        </Fab>
+                    </Tooltip>
+                </div>
+            </div>
+            <div className={classes.keywordsWrapper}>
+                {
+                    'keyword' &&
+                        'keyword'.split(', ').map((option, i) => <Chip
+                            key={i}
+                            label={option}
+                            variant='outlined'
+                            color='primary'
+                            className={classes.metaKeyword}
+                        />)
+                }
+            </div>
+        </div>;
+    }
+}
+
+export default (withStyles(materialStyles)(MetaForm));
