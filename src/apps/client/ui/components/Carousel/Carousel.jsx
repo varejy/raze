98b3f1a31c60d6d895762a298247c0ef6e2b8753
VictoriaVM <victoria.v.movchan@gmail.com>
2019-08-09 13:09:11 +0300

    Добавлена возможность свайпа на главном слайдере

--- src/apps/client/ui/components/Carousel/Carousel.jsx
+++ src/apps/client/ui/components/Carousel/Carousel.jsx
@@ -6,21 +6,25 @@ import classNames from 'classnames';
 import { connect } from 'react-redux';
 
 import calcScrollbarWidth from 'scrollbar-width';
+import Draggable from '../Draggable/Draggable.jsx';
 
 import styles from './Carousel.css';
 
 const TIME_TO_NEXT_SWITCHING = 8000;
 const SWITCHING_DURATION = 800;
+const IGNORE_SWIPE_DISTANCE = 50;
 
 const mapStateToProps = ({ application }) => {
     return {
-        slides: application.mainSlides
+        slides: application.mainSlides,
+        mediaWidth: application.media.width
     };
 };
 
 class Carousel extends Component {
     static propTypes = {
-        slides: PropTypes.array
+        slides: PropTypes.array,
+        mediaWidth: PropTypes.number
     };
 
     static defaultProps = {
@@ -33,9 +37,66 @@ class Carousel extends Component {
         this.animation = false;
         this.maxSlideIndex = this.props.slides.length - 1;
         this.state = {
-            activeSlideIndex: 0
+            activeSlideIndex: 0,
+            startLeft: 0
         };
     }
+
+    maxSlide = this.props.slides.length - 1;
+    maxLeft = this.maxSlide * this.props.mediaWidth;
+
+    componentWillReceiveProps (nextProps) {
+        if (nextProps.mediaWidth !== this.props.mediaWidth) {
+            const { activeSlideIndex } = this.state;
+
+            this.maxLeft = this.maxSlide * nextProps.mediaWidth;
+            this.setState({
+                sliderLeft: activeSlideIndex * nextProps.mediaWidth
+            });
+        }
+    }
+
+    handleDragStart = () => {
+        this.startLeft = this.state.sliderLeft;
+    };
+
+    handleDragProcess = ({ delta: { client: { x: deltaX } } }) => {
+        const nextSliderLeft = this.startLeft - deltaX;
+        const { activeSlideIndex } = this.state;
+        const scrollbarWidth = calcScrollbarWidth();
+
+        if (nextSliderLeft < 0 || nextSliderLeft > this.maxLeft) {
+            return;
+        }
+
+        this.setState({
+            sliderLeft: this.startLeft - deltaX
+        });
+
+        this.sliderTrack.style.left = `-${(this.startLeft - deltaX) + scrollbarWidth * activeSlideIndex}px`;
+    };
+
+    handleDragEnd = ({ delta: { client: { x: deltaX } } }) => {
+        const { slides } = this.props;
+        const { activeSlideIndex } = this.state;
+        const scrollbarWidth = calcScrollbarWidth();
+        const newActiveSlideIndex = deltaX > 0 ? activeSlideIndex - 1 : activeSlideIndex + 1;
+
+        if (Math.abs(deltaX) < IGNORE_SWIPE_DISTANCE || newActiveSlideIndex === -1 || newActiveSlideIndex === slides.length) {
+            return this.setState({
+                sliderLeft: (document.documentElement.clientWidth * activeSlideIndex) + scrollbarWidth * activeSlideIndex
+            });
+        }
+
+        this.startLeft = null;
+        this.setState({
+            sliderLeft: (document.documentElement.clientWidth * newActiveSlideIndex) + scrollbarWidth * newActiveSlideIndex,
+            activeSlideIndex: newActiveSlideIndex
+        });
+
+        clearTimeout(this.sliderTimoutId);
+        this.doSlideSwitch();
+    };
 
     componentDidMount () {
         this.startSlider();
@@ -156,13 +217,21 @@ class Carousel extends Component {
         const { activeSlideIndex } = this.state;
 
         return <div className={styles.carousel}>
-            <div className={styles.sliderTrack} ref={ref => { this.sliderTrack = ref; }}>
-                { slides.map((slide, i) => this.renderSlide(slide, i, slides)) }
-            </div>
-            <div className={styles.dots}>
-                { slides.map((slide, i) =>
-                    <div key={i} className={classNames(styles.dot, { [styles.dotActive]: i === activeSlideIndex })} onClick={this.setActiveSlide(i)} />) }
-            </div>
+            <Draggable
+                onDragStart={this.handleDragStart}
+                onDrag={this.handleDragProcess}
+                onDragEnd={this.handleDragEnd}
+                allowDefaultAction
+                touchable
+            >
+                <div className={styles.sliderTrack} ref={ref => { this.sliderTrack = ref; }}>
+                    { slides.map((slide, i) => this.renderSlide(slide, i, slides)) }
+                </div>
+                <div className={styles.dots}>
+                    { slides.map((slide, i) =>
+                        <div key={i} className={classNames(styles.dot, { [styles.dotActive]: i === activeSlideIndex })} onClick={this.setActiveSlide(i)} />) }
+                </div>
+            </Draggable>
         </div>;
     }
 }
